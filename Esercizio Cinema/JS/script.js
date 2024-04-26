81e9f8145ddfe73b8c343706133ed40e2ed2e8da
AndreaTrapani97 <161830645+AndreaTrapani97@users.noreply.github.com>
2024-04-26 18:13:06 +0200

    sistemato HTML,CSS. Aggunta api completa

--- Esercizio Cinema/JS/script.js
+++ Esercizio Cinema/JS/script.js
@@ -3,13 +3,17 @@ const arrayFilm = [
     "https://www.omdbapi.com/?t=jujutsu+kaisen&y=2021&apikey=93b236e6&",
     "http://www.omdbapi.com/?t=Men+in+Black&y=1997&apikey=93b236e6&"
 ];
-let locandinaFilm = document.querySelector('#locandinaFilm')
+let locandinaFilm = document.querySelector('#locandinaFilm');
 let posizione = 0;
 let arraySalvati = [];
-let informazioniFilm = document.querySelector('#movieInfoJs')
+let informazioniFilm = document.querySelector(".movieInfo");
 
 let btnBack = document.querySelector('#bottoneBack');
 let btnNext = document.querySelector('#bottoneNext');
+
+let movieTitle = document.querySelector(".movieTitle");
+let inputText = document.querySelector(".input");
+let btnCerca = document.querySelector("#btnCerca");
 
 /* ---------------------------------------------- Script ---------------------------------------------- */
 
@@ -23,21 +27,50 @@ arrayFilm.forEach(urlFilm => {
         arraySalvati.push(data);
 
         if(arraySalvati.length == 1){
-            creaCarosello()
+            creaCarosello();
+            
+            
         }
     })
 })
 
 function creaCarosello() {
-
-
     let posterUrl = arraySalvati[posizione].Poster;
-    let posterImg = document.createElement('img');
+  let posterImg = document.createElement('img');
 
-    posterImg.src = posterUrl;
+  let posterTitleText = arraySalvati[posizione].Title;
+  let posterTitle = document.createElement('h2');
 
-    locandinaFilm.innerHTML = '';
-    locandinaFilm.appendChild(posterImg);
+  let posterYearDate = arraySalvati[posizione].Year;
+  let posterYear = document.createElement('p');
+
+  let posterActorsFilm = arraySalvati[posizione].Actors;
+  let posterActors = document.createElement('p');
+
+  let posterRuntimeFilm= arraySalvati[posizione].Runtime;
+  let posterRuntime = document.createElement('p');
+
+  let posterGenreFilm = arraySalvati[posizione].Genre;
+  let posterGenre = document.createElement('p');
+
+  posterImg.src = posterUrl;
+  posterTitle.textContent = posterTitleText;
+  posterYear.textContent = posterYearDate;
+  posterActors.textContent = posterActorsFilm;
+  posterRuntime.textContent = posterRuntimeFilm;
+  posterGenre.textContent = posterGenreFilm;
+
+
+
+  locandinaFilm.innerHTML = '';
+
+  locandinaFilm.appendChild(posterTitle);
+  locandinaFilm.appendChild(posterImg);
+  locandinaFilm.appendChild(posterActors);
+  locandinaFilm.appendChild(posterRuntime);
+  locandinaFilm.appendChild(posterYear);
+  locandinaFilm.appendChild(posterGenre);
+
 }
 
 function back() {
@@ -48,6 +81,7 @@ function back() {
     }
 
     creaCarosello();
+    
 }
 
 btnBack.addEventListener('click', back);
@@ -60,7 +94,22 @@ function next() {
     }
 
     creaCarosello();
+  
 }
 
 btnNext.addEventListener('click', next);
 
+function barraRicerca() {
+    if (inputText.value != ''){
+        let urlDinamico=`http://www.omdbapi.com/?s=${inputText.value}&apikey=93b236e6&"`;
+        fetch(urlDinamico)
+    .then(response => {
+        return response.json()
+    })
+    .then(data => {
+        
+    })
+}
+}
+btnCerca.addEventListener("click",barraRicerca);
+//    http://www.omdbapi.com/?s=your+name.&apikey=93b236e6&",
